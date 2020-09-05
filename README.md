ab2963b5bc572bdcb5d7b44cb56ceabffc96851a
Mehmet Koseoglu <hggm02@gmail.com>
2020-09-05 18:52:25 +1000

    Database model for user added + logic for serving page

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Quaranteam
+
 IT Project
+
+### Setup for Backend
+
+1. Download Node.js
+2. Download Postman (to make requests to backend for testing)
+3. npm i express express-validator config jsonwebtoken mongoose
+4. npm i -D nodemon concurrently
+
+### How to Run Server
+
+npm run server
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+// Allows parsing of req body
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
