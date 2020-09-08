2a996b98f5015a071e581320fd7f064b6969d72b
Winnie76 <1438290294@qq.com>
2020-09-08 18:28:26 +1000

    add files

--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import AuthService from '../auth/auth.service';
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: AuthService.getCurrentUser(),
+    };
+  }
   render() {
     return (
       <React.Fragment>
         <h1>this is dashboard</h1>
+        <button className='btn btn-primary btn-block'>
+          create a portfolio
+        </button>
       </React.Fragment>
     );
   }
