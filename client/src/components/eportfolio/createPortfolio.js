77fe8364877efbbd00cef1cce7640d303883a0cf
Winnie76 <1438290294@qq.com>
2020-09-08 20:41:05 +1000

    Update createPortfolio.js

--- client/src/components/eportfolio/createPortfolio.js
+++ client/src/components/eportfolio/createPortfolio.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import AuthService from '../auth/auth.service';
+import Form from 'react-validation/build/form';
+import Input from 'react-validation/build/input';
 import { Link } from 'react-router-dom';
+
+const required = (value) => {
+  if (!value) {
+    return (
+      <div className='alert alert-danger' role='alert'>
+        This field is required!
+      </div>
+    );
+  }
+};
 
 class CreatePortfolio extends Component {
   constructor(props) {
