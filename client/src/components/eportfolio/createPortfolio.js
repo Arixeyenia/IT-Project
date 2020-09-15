0562b5361c693e682bd5fad24ee511e89040fa9d
Ajay Singh <assin.student.unimelb.edu.au>
2020-09-15 16:39:16 +1000

    Placed elements and basic styling for Create Portfolio

--- client/src/components/eportfolio/createPortfolio.js
+++ client/src/components/eportfolio/createPortfolio.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, {Fragment, Component } from 'react';
+import { Box, Button, Typography, Grid } from '@material-ui/core';
 import AuthService from '../auth/auth.service';
 import Form from 'react-validation/build/form';
 import Input from 'react-validation/build/input';
@@ -24,29 +25,19 @@ class CreatePortfolio extends Component {
   }
   render() {
     return (
-      <React.Fragment>
-        <h1>choose your portfolio template and start!</h1>
-        <Link to={'/template1'}>
-          <button type='button' className='btn btn-primary btn-block'>
-            use template1
-          </button>
-        </Link>
-        <Link to={'/template2'}>
-          <button type='button' className='btn btn-primary btn-block'>
-            use template2
-          </button>
-        </Link>
-        <Link to={'/template3'}>
-          <button type='button' className='btn btn-primary btn-block'>
-            use template3
-          </button>
-        </Link>
-        <Link to={'/templateBlank'}>
-          <button type='button' className='btn btn-primary btn-block'>
-            use blank template
-          </button>
-        </Link>
-      </React.Fragment>
+      <Fragment>
+        <Box className='select-template fontg1' bgcolor = '#FFFFFF'>
+          <button style={{ marginLeft: '150px'}} className="template-button">Professional</button>
+          <button style={{ marginLeft: '20px' }} className="template-button">Template2</button>
+          <button style={{ marginLeft: '20px' }} className="template-button">Template3</button>
+          <button style={{ marginLeft: '20px' }} className="template-button">Blank</button>
+        </Box>
+        <Box className='select-template fontg1' bgcolor = '#FFFFFF'>
+          <Typography style={{ marginLeft: '150px'}}>Pick a template</Typography>
+          <div className="rectangle"/>
+          <Button style={{ marginLeft: '20px' }} variant='contained' color='primary'> OK </Button>
+        </Box>
+      </Fragment>
     );
   }
 }
