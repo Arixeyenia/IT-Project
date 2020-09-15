97566007c87b628a49e066e27d176ecf4b08ea47
Ajay Singh <assin.student.unimelb.edu.au>
2020-09-15 17:29:20 +1000

    Added an illustration and CTA when no temlpate is selected

--- client/src/components/eportfolio/createPortfolio.js
+++ client/src/components/eportfolio/createPortfolio.js
@@ -1,9 +1,7 @@
 import React, {Fragment, Component } from 'react';
 import { Box, Button, Typography, Grid } from '@material-ui/core';
 import AuthService from '../auth/auth.service';
-import Form from 'react-validation/build/form';
-import Input from 'react-validation/build/input';
-import { Link } from 'react-router-dom';
+import image from '../../images/pick.png';
 
 const required = (value) => {
   if (!value) {
@@ -35,8 +33,19 @@ class CreatePortfolio extends Component {
         <Box className='select-template fontg1' bgcolor = '#FFFFFF'>
           <Typography style={{ marginLeft: '150px'}}>Pick a template</Typography>
           <div className="rectangle"/>
-          <Button style={{ marginLeft: '20px' }} variant='contained' color='primary'> OK </Button>
+          <Button style={{marginBottom: '10px'}} variant='contained' size='sm' color='primary'> OK </Button>
         </Box>
+        <Box className='content half gray6 fontg1'>
+          <Box className='left'>
+            <Typography style={{ marginTop: '120px'}} variant='h1'>Choose a template</Typography>
+            <Typography variant='h6'>
+              Pick one of the options above to see if it works for you.
+            </Typography>
+          </Box>
+        <Box className='right'>
+          <img src={image} alt='Illustration'></img>
+        </Box>
+      </Box>
       </Fragment>
     );
   }
