c42ae38e5d82629b89825b8b93ccbbccbb019a4b
Juwey <juweytan@gmail.com>
2020-09-30 19:12:14 +1000

    Updated dashboard to makeStyles and split components in dashboard into different files

--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -1,13 +1,16 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Typography, Divider, Box, List, Card, CardContent, CardHeader, IconButton, Icon, CardActionArea, Menu, MenuItem, GridList, GridListTile, Popover, Button } from '@material-ui/core';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { Typography, Box, List, Card, CardContent, Icon, CardActionArea, GridList, GridListTile } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add'
 import {getUserEPortfolios, getEPortfolioThumbnail, deletePortfolio} from '../../actions/eportfolio';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Category from './DashboardCategory';
+import IndividualMenu from './DashboardMenu';
+import useStyles from './DashboardStyles';
 
 const Dashboard = ({getUserEPortfolios, userEPortfolios, getEPortfolioThumbnail, eportfolioThumbnails, deletePortfolio}) => {
+  const classes = useStyles();
   useEffect(() => {
     if (userEPortfolios.length == 0){
       getUserEPortfolios();
@@ -33,13 +36,13 @@ const Dashboard = ({getUserEPortfolios, userEPortfolios, getEPortfolioThumbnail,
     <Box className="content">
       <Typography variant="h1">Welcome to your dashboard</Typography>
       <Category title="Your existing ePortfolios"></Category>
-      <GridList className="portfolioList">
+      <GridList className={classes.portfolioList}>
         {DisplayPortfolioItem(arrayOfPortfolioObjects, deletePortfolio)}
-        <GridListTile className="portfolioListItem" key="last">
-            <Card raised={true} className="portfolioCard MuiButton-root">
+        <GridListTile className={classes.portfolioListItem} key="last">
+            <Card raised={true} className={`${classes.portfolioCard} "MuiButton-root"`}>
               <CardActionArea><Link to="/create-eportfolio">
-                <Box className="addPortfolio">
-                  <Icon aria-label="settings" className="addPortfolioIcon">
+                <Box className={classes.addPortfolio}>
+                  <Icon aria-label="settings" className={classes.addPortfolioIcon}>
                     <AddIcon fontSize="large"/>
                   </Icon>
                 </Box>
@@ -52,27 +55,18 @@ const Dashboard = ({getUserEPortfolios, userEPortfolios, getEPortfolioThumbnail,
     </Box>
   );
 }
-function Category(props) {
-  return (
-    <Box className="category">
-      <Typography noWrap variant="body1">{props.title}</Typography>
-      <Box>
-        <Divider light className="categoryLine"/>
-      </Box>
-    </Box>
-  );
-}
+
 
 // Component to map each tile
 function DisplayPortfolioItem(arrayOfPortfolioObjects, deletePortfolio) {
-  
+  const classes = useStyles();
   return(
     arrayOfPortfolioObjects.map((object, i) => (
-      <GridListTile className="portfolioListItem" key={object.portfolio._id}>
-        <Card raised={true} className="portfolioCard">
+      <GridListTile className={classes.portfolioListItem} key={object.portfolio._id}>
+        <Card raised={true} className={classes.portfolioCard}>
           <IndividualMenu i={i} object={object} deletePortfolio={deletePortfolio}/>
-          <img src={object.thumbnail} alt="Portfolio Thumbnail" className="cardThumbnail"></img>
-          <CardContent className="overlayPortfolioItem">
+          <img src={object.thumbnail} alt="Portfolio Thumbnail" className={classes.cardThumbnail}></img>
+          <CardContent className={classes.overlayPortfolioItem}>
             <Typography variant="body1" className="fontg6">{object.portfolio.name}</Typography>
           </CardContent>
         </Card>
@@ -80,76 +74,6 @@ function DisplayPortfolioItem(arrayOfPortfolioObjects, deletePortfolio) {
       </GridListTile>
     ))
   );
-}
-
-// Currently using this component for the button and drop down menu
-function IndividualMenu(props) {
-  const history = useHistory();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const [popoverAnchor, setPopoverAnchor] = React.useState(null);
-
-  const openPopover = (event, id) => {
-    setPopoverAnchor(event.currentTarget);
-    var index = window.location.href.lastIndexOf('/');
-    setUrl(window.location.href.slice(0, index)+ '/' + 'view/' + id);
-  };
-
-  const popoverClose = () => {
-    setPopoverAnchor(null);
-  };
-
-  // TODO: Copy success
-  var [url, setUrl] = React.useState('');
-  const copyClipboardLink = (elementId) => {
-    document.getElementById(elementId).value = url;
-    var textToCopy = document.getElementById(elementId);
-    var range = document.createRange();
-    range.selectNode(textToCopy);
-    var selection = window.getSelection();
-    selection.removeAllRanges();
-    selection.addRange(range);
-    document.execCommand("copy");
-  }
-
-  return(
-    <Box className="card-action-section">
-      <CardHeader className="portfolio-card-header" action={
-        <IconButton aria-label="settings" aria-controls={"menu-"+props.object.portfolio._id} onClick={handleClick}>
-          <MoreVertIcon />
-        </IconButton>
-      }>
-      </CardHeader>
-      <Menu id={"menu-"+props.i}
-              anchorEl={anchorEl}
-              keepMounted
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-              className="PortfolioCard-Menu">
-          <MenuItem onClick={() => history.push('/view/' + props.object.portfolio._id)}>View</MenuItem>
-          <MenuItem onClick={() => history.push('/edit/' + props.object.portfolio._id)}>Edit</MenuItem>
-          <MenuItem onClick={() => {props.deletePortfolio(props.object.portfolio._id)}}>Delete</MenuItem>
-          <MenuItem onClick={(event)=>openPopover(event, props.object.portfolio._id)}>Get link</MenuItem>
-      </Menu>
-      <Popover id={"popover-"+props.i}
-              anchorEl={popoverAnchor}
-              onClose={popoverClose}
-              open={Boolean(popoverAnchor)}
-              className="copylink-popover">
-        <textarea disabled id={"text-"+props.i} value={url}></textarea>
-        <Button variant="contained" color="primary" onClick={() => {copyClipboardLink("text-"+props.i)}}>Copy to clipboard</Button>
-      </Popover>
-    </Box>
-  );
-
 }
 
 Dashboard.propTypes = {
