248bfe08e4f4bd55d60858794b722a1b3c88aabc
Juwey <juweytan@gmail.com>
2020-09-30 20:16:57 +1000

    Change to function component and makeStyles

--- client/src/components/eportfolio/PickTemplate.js
+++ client/src/components/eportfolio/PickTemplate.js
@@ -5,40 +5,53 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createPortfolio } from '../../actions/eportfolio'
 import store from '../../store'
+import { makeStyles } from '@material-ui/core/styles';
 
-class PickTemplate extends Component {
-  constructor(props) {
-    super(props);
+const useStyles = makeStyles((theme) => ({
+  category: {
+    display: 'inline-table',
+  },
+  categoryTypography: {
+    display: 'table-cell',
+  },
+  categoryDiv: {
+    display: 'table-cell',
+    width: '100%',
+  },
+  categoryLine: {
+    marginBottom: '5px',
   }
-  render() {
-    return (
-      <Fragment>
-        <Box className='content template-selection fontg1 gray6'>
-          <Box className="template-button-section">
-            <Button variant="contained" className="template-button">Blank</Button>
-          </Box>
-          <Box className="category">
-            <Typography noWrap variant="body1">Pick a template</Typography>
-            <Box>
-              <Divider light className="categoryLine"/>
-            </Box>
-            <Button style={{marginBottom: '10px'}} variant='contained' color='primary' onClick={()=>{this.props.createPortfolio(store.getState().eportfolio.createPortfolioName)}}>CREATE</Button>
-          </Box>
+}));
+
+const PickTemplate = ({createPortfolio}) => {
+  const classes = useStyles();
+  return (
+    <Fragment>
+      <Box className='content template-selection fontg1 gray6'>
+        <Box className="template-button-section">
+          <Button variant="contained" className="template-button">Blank</Button>
         </Box>
-        <Box className='content half gray6 fontg1'>
-          <Box className='left'>
-            <Typography variant='h1'>Choose a template</Typography>
-            <Typography variant='h6'>
-              Pick one of the options above to see if it works for you.
-            </Typography>
+        <Box className={classes.category}>
+          <Typography noWrap variant="body1" className={classes.categoryTypography}>Pick a template</Typography>
+          <Box className={classes.categoryDiv}>
+            <Divider light className={classes.categoryLine}/>
           </Box>
-          <Box className='right'>
-            <img src={image} alt='Illustration'></img>
-          </Box>
+          <Button style={{marginBottom: '10px'}} variant='contained' color='primary' onClick={()=>{createPortfolio(store.getState().eportfolio.createPortfolioName)}}>CREATE</Button>
         </Box>
-      </Fragment>
-    );
-  }
+      </Box>
+      <Box className='content half gray6 fontg1'>
+        <Box className='left'>
+          <Typography variant='h1'>Choose a template</Typography>
+          <Typography variant='h6'>
+            Pick one of the options above to see if it works for you.
+          </Typography>
+        </Box>
+        <Box className='right'>
+          <img src={image} alt='Illustration'></img>
+        </Box>
+      </Box>
+    </Fragment>
+  );
 }
 
 PickTemplate.propTypes = {
