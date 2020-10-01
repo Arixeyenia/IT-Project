dd713fb4aa2c84b7d6b4cce8de10c0bc91ea5270
Winnie76 <1438290294@qq.com>
2020-10-02 00:16:54 +1000

    Update Comment.js

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const CommentSchema = new mongoose.Schema({
   from: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: String,
     ref: 'user',
   },
   name: {
