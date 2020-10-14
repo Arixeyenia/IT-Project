243248a96f78eacbe9debc07c5f7d3a127fcae46
Juwey <juweytan@gmail.com>
2020-10-14 23:13:59 +1100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -25,8 +25,8 @@ app.use('/api/comment', require('./routes/api/comment'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('../client/build'));
-
+  app.use(express.static('../../client/build'));
+  console.log(__dirname);
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', '..', 'client', 'build', 'index.html'));
   });
