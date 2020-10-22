eb6fe027b0f7f11b41287972f200f1e64c1093c9
Mehmet Koseoglu <hggm02@gmail.com>
2020-10-22 13:42:40 +1100

    Update media.js

--- server/routes/api/media.js
+++ server/routes/api/media.js
@@ -65,6 +65,23 @@ router.post('/', [auth, upload.single('file')], (req, res) => {
   res.redirect('/');
 });
 
+// @route GET /media/files
+// @desc  Display all files in JSON
+// @access  Public
+app.get('/files', (req, res) => {
+  gfs.files.find().toArray((err, files) => {
+    // Check if files
+    if (!files || files.length === 0) {
+      return res.status(404).json({
+        err: 'No files exist',
+      });
+    }
+
+    // Files exist
+    return res.json(files);
+  });
+});
+
 // @route   GET api/media/:filename
 // @desc    Get file object
 // @access  Private
